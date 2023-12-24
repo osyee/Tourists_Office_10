bf2715e0b08377e121bb8a2366fd3ca4dd9d6f06
enass2023 <eng.enas.mohammd97@gmail.com>
2023-12-24 12:06:41 +0200

    add modelreletion

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -10,5 +10,33 @@ use App\Models\City;
 class Ticket extends Model
 {
     use HasFactory;
-  
+    protected $fillable = [
+        'company_id',
+        'city_id',
+        'date_s',
+        'date_e'
+    ];
+    protected $casts = [
+        "company_id"=> "integer",
+        "city_id"=> "integer",
+        "date_s"=> "date",
+        "date_e"=> "date"
+    ];
+
+public function company(){
+
+ return $this->belongsTo(Company::class);
+}
+public function city(){
+    
+ return $this->belongsTo(City::class);
+}
+
+
+public function bookings()
+{
+
+return $this->hasMany(Booking::class);
+
+}
 }
